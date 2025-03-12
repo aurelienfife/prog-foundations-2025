524d8cd1c72f48837cad5fb2e23092c38ed22b2c
aurelienfife <161948862+aurelienfife@users.noreply.github.com>
2025-03-12 11:55:36 +0000

    String demo week 6

---
+++ Week6-Strings/src/week6strings/Main.java
@@ -0,0 +1,24 @@
+package week6strings;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		String ex = String.format("My answer is %.2f", 46.65734);
+		System.out.println(ex);
+		
+		String employee = String.format("%20s %30s %30.2f", "John Smith", "Fife College", 12.76);
+		System.out.println(employee);
+		
+		String a = "cat";
+		System.out.printf("He insists he\\ sees a %s \n", "Ghost");
+		System.out.printf("He insists he sees a %s \n", a);
+		
+		
+		
+		
+	}
+
+}
