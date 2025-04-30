fe69f586487d2a9f1a0cff2d948edca03aee3952
aurelienfife <161948862+aurelienfife@users.noreply.github.com>
2025-04-30 12:06:55 +0100

    updated

--- FileIO/src/fileio/FileIODemo.java
+++ FileIO/src/fileio/FileIODemo.java
@@ -1,10 +1,41 @@
 package fileio;
 
+// Libraries needed
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 public class FileIODemo {
 
 	// Code for demo goes here
 	public void run() {
 		
+		// Open the CSV file and create a file stream
+		File f = new File("temps.csv");
+		// Create the array before try { } otherwise it will not persist
+		ArrayList<String> fileLines = new ArrayList<String>();
+		
+		try {
+			// Create the scanner here - can potentially crash
+			Scanner s = new Scanner(f);
+			// All the code needing the scanner goes inside the try
+			// You can iterate the scanner - nextLine() returns the next text line
+			// and moves forward into the file
+			while(s.hasNextLine()) {
+				fileLines.add(s.nextLine());
+			}
+			s.close();
+		}
+		catch (FileNotFoundException e) {
+			// If the file doesn't exist, handle it and stop program
+			System.out.println("Error: file not found - " + e.getMessage());
+			System.exit(1);
+		}
+		
+		
+		// Rest of the program goes there
+		System.out.println(fileLines);
 	}
 	
 }
--- FileIO/src/fileio/Main.java
+++ FileIO/src/fileio/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		// Create instance of demo class and run
 		FileIODemo d = new FileIODemo();
 		d.run();
 
